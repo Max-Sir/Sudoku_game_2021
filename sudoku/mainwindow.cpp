2834b59d906b1fae260c9ecd7aff2aeaf3a2c339
Vlad Kumishche <69390727+Vlad-Kumishche@users.noreply.github.com>
2021-10-06 19:43:07 +0300

    Update mainwindow.cpp

--- sudoku/mainwindow.cpp
+++ sudoku/mainwindow.cpp
@@ -13,3 +13,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::load_prob(int clear)
+{
+    for(int i=0;i<9;++i)
+        for(int j=0;j<9;++j)
+        {
+            if(prob.mtr[i][j])
+                m.mtr[i][j]=1<<prob.mtr[i][j];
+            else
+                m.mtr[i][j]=0;
+        }
+}
