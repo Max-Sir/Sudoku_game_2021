a70631bd22c3141b66516e8c7179d63f1b0116c1
Vlad Kumishche <69390727+Vlad-Kumishche@users.noreply.github.com>
2021-10-06 19:59:37 +0300

    load_prob intermediate implementation

--- sudoku/mainwindow.cpp
+++ sudoku/mainwindow.cpp
@@ -13,3 +13,20 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::load_prob(int clear)
+{
+    if(clear)
+    {
+        //clear all history record
+        history_temp=0;
+        history.clear();
+        history.append(m);
+        hx.clear();
+        hx.append(select_x);
+        hy.clear();
+        hy.append(select_y);
+        hmark.clear();
+        hmark.append(mark);
+    }
+    refresh_xy(1);
+}
