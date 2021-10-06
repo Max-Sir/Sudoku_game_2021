4f2cc460df5ba7e84eb5fecfc7c3761bd15ae055
Vlad Kumishche <69390727+Vlad-Kumishche@users.noreply.github.com>
2021-10-06 20:29:44 +0300

    load_prob implemented

--- sudoku/mainwindow.cpp
+++ sudoku/mainwindow.cpp
@@ -15,6 +15,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::load_prob(int clear)
 {
+    for(int i=0;i<9;++i)
+        for(int j=0;j<9;++j)
+        {
+            if(prob.mtr[i][j])
+                m.mtr[i][j]=1<<prob.mtr[i][j];
+            else
+                m.mtr[i][j]=0;
+        }
+    for(int i=0;i<9;++i)
+        for(int j=0;j<9;++j)
+        {
+            mark.mtr[i][j]=0;
+            if(level==0)
+                prob.mtr[i][j]=m.mtr[i][j]=0;
+        }
     if(clear)
     {
         //clear all history record
