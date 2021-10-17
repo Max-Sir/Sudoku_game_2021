7fcf30d40c79fdb0e4ecfc5224f5598c6a1c874d
Vlad Kumishche <69390727+Vlad-Kumishche@users.noreply.github.com>
2021-10-17 21:42:49 +0300

    Update mainwindow.h

--- sudoku/mainwindow.h
+++ sudoku/mainwindow.h
@@ -25,6 +25,7 @@ protected:
     void continuegame();
     void load_prob(int clear);
     void show_solution(int origin);
+    void refresh_xy(int add);
     void show_tip();
     int checkavailable(int,int);
     QString change_bin_number_into_String(int);
